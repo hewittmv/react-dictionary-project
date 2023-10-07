fee7df6f1ccfafb06cbe0bee19d866bf6bfde44d
hewittmv <111692506+hewittmv@users.noreply.github.com>
2023-10-07 19:40:50 +0100

    added container

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <footer className="footer">
-        <a
-          className="github-link"
-          href="https://github.com/hewittmv/react-dictionary-project"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Open Source Code by VHM
-        </a>
-        <img src={logo} className="react-logo" width="50px" alt="logo" />
-      </footer>
+      <div className="container">
+        <footer className="footer">
+          <a
+            className="github-link"
+            href="https://github.com/hewittmv/react-dictionary-project"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Open Source Code by VHM
+          </a>
+          <img src={logo} className="react-logo" width="50px" alt="logo" />
+        </footer>
+      </div>
     </div>
   );
 }

716d7bef63c1c7eba74614e553a2d09d90a466e9
hewittmv <111692506+hewittmv@users.noreply.github.com>
2023-10-07 20:51:23 +0100

    Add search form button

--- src/Search.js
+++ src/Search.js
@@ -1,6 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Search.css";
 
 export default function Search() {
-  return <div className="Search">Dictionary</div>;
+  let [keyword, SetKeyword] = useState("");
+
+  function Search(event) {
+    event.preventDefault();
+    alert(`Searching for ${keyword}`);
+  }
+  function handleKeywordChange(event) {
+    SetKeyword(event.target.value);
+  }
+  return (
+    <div className="Search">
+      Dictionary
+      <form onSubmit={Search}>
+        <input type="Search" autofocus="true" onChange={handleKeywordChange} />
+        <input type="submit" value="Look It Up" />
+      </form>
+    </div>
+  );
 }

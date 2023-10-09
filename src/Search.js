646d304a80977a69a1a89b90ab9110011d372975
hewittmv <111692506+hewittmv@users.noreply.github.com>
2023-10-09 10:17:42 +0100

    integrated dictionary API

--- src/Search.js
+++ src/Search.js
@@ -1,12 +1,19 @@
 import React, { useState } from "react";
 import "./Search.css";
+import axios from "axios";
 
 export default function Search() {
   let [keyword, SetKeyword] = useState("");
 
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+
   function Search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword}`);
+    let apiKey = "o00a45f3c7757cta20b5ccabe4f8ba48";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
   }
   function handleKeywordChange(event) {
     SetKeyword(event.target.value);

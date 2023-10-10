09015dab82173578c37f0745969f8c3c3b505411
hewittmv <111692506+hewittmv@users.noreply.github.com>
2023-10-10 14:41:59 +0100

    Add synonym feature

--- src/Synonym.js
+++ src/Synonym.js
@@ -1,1 +1,19 @@
+import React from "react";
 
+export default function Synonym(props) {
+  if (props.synonym) {
+    return (
+      <div className="synonym">
+        {" "}
+        Synonym:
+        <ul>
+          {props.synonym.map(function (synonym, index) {
+            return <li key={index}>{synonym}</li>;
+          })}
+        </ul>
+      </div>
+    );
+  } else {
+    return null;
+  }
+}

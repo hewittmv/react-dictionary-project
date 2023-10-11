dc8e2278ec2e3ee21464041ce7abd8b22557f462
hewittmv <111692506+hewittmv@users.noreply.github.com>
2023-10-11 13:25:16 +0100

    set search function to receive new DefaultKeyword property sent from App.js

--- src/Search.js
+++ src/Search.js
@@ -3,7 +3,7 @@ import "./Search.css";
 import axios from "axios";
 import Results from "./Results";
 
-export default function Search() {
+export default function Search(props) {
   let [keyword, SetKeyword] = useState("");
   let [results, setResults] = useState(null);
   let [results1, setResults1] = useState(null);

02b843a4720ee298086109ed1bbfec487c9f9e67
hewittmv <111692506+hewittmv@users.noreply.github.com>
2023-10-12 15:27:49 +0100

    Update container styling

--- src/App.js
+++ src/App.js
@@ -9,18 +9,18 @@ function App() {
         <main>
           <Search defaultKeyword="autumn" />
         </main>
-        <footer className="footer">
-          <a
-            className="github-link"
-            href="https://github.com/hewittmv/react-dictionary-project"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Open Source Code by VHM
-          </a>
-          <img src={logo} className="react-logo" width="50px" alt="logo" />
-        </footer>
       </div>
+      <footer className="footer">
+        <a
+          className="github-link"
+          href="https://github.com/hewittmv/react-dictionary-project"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Open Source Code by VHM
+        </a>
+        <img src={logo} className="react-logo" width="50px" alt="logo" />
+      </footer>
     </div>
   );
 }

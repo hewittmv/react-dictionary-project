c67c125fb9333777dbc1bc87895f6e5ee240e927
hewittmv <111692506+hewittmv@users.noreply.github.com>
2023-10-14 19:37:16 +0100

    wrap app inside a container

--- src/App.js
+++ src/App.js
@@ -5,26 +5,28 @@ import Letters from "./Letters";
 
 function App() {
   return (
-    <div className="App">
-      <div class="level-1">
-        <Letters />
-        <div id="container-1">
-          <main>
-            <Search defaultKeyword="autumn" />
-          </main>
-        </div>
-      </div>{" "}
-      <footer className="footer">
-        <a
-          className="github-link"
-          href="https://github.com/hewittmv/react-dictionary-project"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Open Source Code by VHM
-        </a>
-        <img src={logo} className="react-logo" width="50px" alt="logo" />
-      </footer>
+    <div id="container-0">
+      <div className="App">
+        <div class="level-1">
+          <Letters />
+          <div id="container-1">
+            <main>
+              <Search defaultKeyword="autumn" />
+            </main>
+          </div>
+        </div>{" "}
+        <footer className="footer">
+          <a
+            className="github-link"
+            href="https://github.com/hewittmv/react-dictionary-project"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Open Source Code by VHM
+          </a>
+          <img src={logo} className="react-logo" width="50px" alt="logo" />
+        </footer>
+      </div>
     </div>
   );
 }

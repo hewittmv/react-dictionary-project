9de24f4d6f5c3ad16bbe97c5c58bc11397e9f3b3
hewittmv <111692506+hewittmv@users.noreply.github.com>
2023-10-15 20:16:44 +0100

    make letters responsive on smaller devices

--- src/Photos.js
+++ src/Photos.js
@@ -7,25 +7,29 @@ export default function Photos() {
       <div className="image-one">
         <div className="cube w">
           {" "}
-          W<span className="number"> 4</span>{" "}
+          <span className="letter-w">W</span>
+          <span className="number"> 4</span>{" "}
         </div>
       </div>
       <div className="image-two">
         <div className="cube o">
           {" "}
-          O<span className="number"> 1</span>{" "}
+          <span className="letter-o">O</span>
+          <span className="number"> 1</span>{" "}
         </div>
       </div>
       <div className="image-three">
         <div className="cube r">
           {" "}
-          R<span className="number"> 1</span>{" "}
+          <span className="letter-r">R</span>
+          <span className="number"> 1</span>{" "}
         </div>
       </div>
       <div className="image-four">
         <div className="cube d">
           {" "}
-          D<span className="number"> 2</span>{" "}
+          <span className="letter-d">D</span>
+          <span className="number"> 2</span>{" "}
         </div>
       </div>
     </div>

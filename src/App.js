ab2601eb04a9a6163461aced726ff9d3c8e92225
hewittmv <111692506+hewittmv@users.noreply.github.com>
2023-10-16 22:22:41 +0100

    testing footer design

--- src/App.js
+++ src/App.js
@@ -20,6 +20,39 @@ function App() {
           </a>
           <img src={logo} className="react-logo" width="50px" alt="logo" />
         </div>
+        <footer>
+          <div className="Dream d-flex">
+            <div className="cube-d">
+              {" "}
+              D<span className="number"> 2</span>{" "}
+            </div>
+            <span className="word-dream">
+              <strong>Dream </strong>
+            </span>
+            <span className="partOfSpeech-dream">Noun</span> <br />
+            <span className="dream-definition">
+              <span className="speech-marks">"</span>
+              Thoughts, images, sensations <br />
+              that occur during sleep
+              <span className="speech-marks">"</span>
+            </span>
+          </div>
+          <div className="Nascent d-flex">
+            <div className="cube-n">
+              {" "}
+              N<span className="number"> 1</span>{" "}
+            </div>
+            <span className="word-nascent">
+              <strong>Nascent </strong>
+            </span>
+            <span className="partOfSpeech-nascent">Adjective</span> <br />
+            <span className="nascent-definition">
+              <span className="speech-marks">"</span> just coming into existence
+              & beginning <br /> to display signs of future potential
+              <span className="speech-marks">"</span>
+            </span>
+          </div>
+        </footer>
       </div>
     </div>
   );

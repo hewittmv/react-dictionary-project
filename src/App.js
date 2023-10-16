8972a4b696c235e014ed63a532c10e590a2fb126
hewittmv <111692506+hewittmv@users.noreply.github.com>
2023-10-16 16:49:48 +0100

    center align container

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Search from "./Search";
-import Letters from "./Letters";
 
 function App() {
   return (
     <div className="App">
-      <div class="container" id="container-zero">
-        <header className="header">
-          <div className="Nascent">
-            <span className="word-nascent">
-              <strong>Nascent </strong>
-            </span>
-            <span className="partOfSpeech-nascent">Adjective</span> <br />
-            <span className="nascent-definition">
-              <span className="speech-marks">"</span> just coming into existence
-              & beginning <br /> to display signs of future potential
-              <span className="speech-marks">"</span>
-            </span>
-          </div>
-        </header>
-        <Letters />
+      <div className="container d-flex justify-content-center">
+        <header className="header"></header>
+
         <div id="container-1">
           <main>
             <Search defaultKeyword="autumn" />
@@ -36,24 +23,7 @@ function App() {
           <img src={logo} className="react-logo" width="50px" alt="logo" />
         </div>
 
-        <footer className="footer">
-          <div className="cube-d">
-            {" "}
-            D<span className="number"> 2</span>{" "}
-          </div>
-          <div className="Dream">
-            <span className="word-dream">
-              <strong>Dream </strong>
-            </span>
-            <span className="partOfSpeech-Dream">Noun</span> <br />
-            <span className="dream-definition">
-              <span className="speech-marks">"</span>
-              Thoughts, images, sensations <br />
-              that occur during sleep
-              <span className="speech-marks">"</span>
-            </span>
-          </div>
-        </footer>
+        <footer className="footer"></footer>
       </div>
     </div>
   );

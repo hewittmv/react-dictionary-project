eb3cc9f914831a2da3c6496ce609e6027c1cb882
hewittmv <111692506+hewittmv@users.noreply.github.com>
2023-10-16 16:19:33 +0100

    move Github link inside app container

--- src/App.js
+++ src/App.js
@@ -25,6 +25,15 @@ function App() {
           <main>
             <Search defaultKeyword="autumn" />
           </main>
+          <a
+            className="github-link"
+            href="https://github.com/hewittmv/react-dictionary-project"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Open Source Code by VHM
+          </a>
+          <img src={logo} className="react-logo" width="50px" alt="logo" />
         </div>
 
         <footer className="footer">
@@ -44,15 +53,6 @@ function App() {
               <span className="speech-marks">"</span>
             </span>
           </div>
-          <a
-            className="github-link"
-            href="https://github.com/hewittmv/react-dictionary-project"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Open Source Code by VHM
-          </a>
-          <img src={logo} className="react-logo" width="50px" alt="logo" />
         </footer>
       </div>
     </div>

fea62b39237fe40813481b2a3e2da34d22bde79d
hewittmv <111692506+hewittmv@users.noreply.github.com>
2023-10-24 20:18:55 +0100

    Adjust styling on mobile

--- src/Footer.js
+++ src/Footer.js
@@ -3,22 +3,20 @@ import "./Footer.css";
 
 export default function Footer() {
   return (
-    <footer className="d-flex w-100">
-      <div className="Dream d-flex justify-content-center align-items-center w-100">
-        <div className="cube-d">
-          {" "}
-          D<span className="dream-number"> 2</span>{" "}
-        </div>
-        <div className="dream-noun">
-          <span className="word-dream">
-            <strong>Dream </strong>
-          </span>{" "}
-          <span className="partOfSpeech-dream">Noun</span>
-        </div>
-        <div className="dream-definition d-flex align-items-center">
-          "Thoughts, images, sensations that occur during sleep"
-        </div>
+    <div className="Dream-wrapper">
+      <div className="cube-d">
+        {" "}
+        D<span className="dream-number"> 2</span>{" "}
       </div>
-    </footer>
+      <div className="dream-noun">
+        <span className="word-dream">
+          <strong>Dream </strong>
+        </span>{" "}
+        <span className="partOfSpeech-dream">Noun</span>
+      </div>
+      <div className="dream-definition d-flex align-items-center text-center">
+        "Thoughts, images, sensations that occur during sleep"
+      </div>
+    </div>
   );
 }
--- src/Header.js
+++ src/Header.js
@@ -3,22 +3,20 @@ import "./Header.css";
 
 export default function Header() {
   return (
-    <header className="w-100 d-flex">
-      <div className="Nascent d-flex  justify-content-center w-100">
-        <div className="cube-n mt-2">
-          {" "}
-          N<span className="nascent-number"> 1</span>{" "}
-        </div>
-        <div className="nascent-adj">
-          <span className="word-nascent">
-            <strong>Nascent </strong>
-          </span>{" "}
-          <span className="partOfSpeech-nascent">Adjective</span>
-        </div>
-        <div className="nascent-definition d-flex align-items-center">
-          "Just coming into existence & beginning to display future potential"
-        </div>
+    <div className="Nascent-wrapper">
+      <div className="cube-n">
+        {" "}
+        N<span className="nascent-number"> 1</span>{" "}
       </div>
-    </header>
+      <div className="nascent-adj">
+        <span className="word-nascent">
+          <strong>Nascent </strong>
+        </span>{" "}
+        <span className="partOfSpeech-nascent">Adjective</span>
+      </div>
+      <div className="nascent-definition d-flex align-items-center text-center">
+        "Just coming into existence & beginning to display future potential"
+      </div>
+    </div>
   );
 }

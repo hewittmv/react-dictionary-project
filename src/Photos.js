70bbf6c4def870f853b71d3f98bd126741fe0b62
hewittmv <111692506+hewittmv@users.noreply.github.com>
2023-10-25 13:37:28 +0100

    Adjusted class names

--- src/Photos.js
+++ src/Photos.js
@@ -35,28 +35,28 @@ export default function Photos(props) {
     return (
       <div className="images">
         <div className="image-one">
-          <div className="cube w">
+          <div className="cube-w">
             {" "}
             <span className="letter-w">W</span>
             <span className="word-number"> 4</span>{" "}
           </div>
         </div>
         <div className="image-two">
-          <div className="cube o">
+          <div className="cube-o">
             {" "}
             <span className="letter-o">O</span>
             <span className="word-number"> 1</span>{" "}
           </div>
         </div>
         <div className="image-three">
-          <div className="cube r">
+          <div className="cube-r">
             {" "}
             <span className="letter-r">R</span>
             <span className="word-number"> 1</span>{" "}
           </div>
         </div>
         <div className="image-four">
-          <div className="cube d">
+          <div className="cube-dd">
             {" "}
             <span className="letter-d">D</span>
             <span className="word-number"> 2</span>{" "}
